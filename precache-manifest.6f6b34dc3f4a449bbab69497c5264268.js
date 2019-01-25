74720735a4d95ca202c9af176e2ca5df8d905a0b
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-25 17:25:51 -0600

    Updates

---
+++ precache-manifest.6f6b34dc3f4a449bbab69497c5264268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c13ef4359f0b536b667",
+    "url": "/front-end-project-week/static/js/runtime~main.9c13ef43.js"
+  },
+  {
+    "revision": "be49210f055b213ffdc8",
+    "url": "/front-end-project-week/static/js/main.be49210f.chunk.js"
+  },
+  {
+    "revision": "757bc0c40c5a627ed641",
+    "url": "/front-end-project-week/static/js/1.757bc0c4.chunk.js"
+  },
+  {
+    "revision": "be49210f055b213ffdc8",
+    "url": "/front-end-project-week/static/css/main.d2922a25.chunk.css"
+  },
+  {
+    "revision": "38ce1fa2d3fccfce86242fe19ee896ba",
+    "url": "/front-end-project-week/index.html"
+  }
+];
